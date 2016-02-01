e06b5dbca507240aa03eb74f8c966d0e71c50083
chance garcia <develop@chancegarcia.com>
2016-02-01 17:01:37 -0500

    casebox newsfeed 0.3.1 changes

---
+++ src/Box/Model/Connection/Response/AuthenticationResponse.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * @package     Box
+ * @subpackage  Box_Connection
+ * @author      Chance Garcia
+ * @copyright   (C)Copyright 2013 Chance Garcia, chancegarcia.com
+ *
+ *    This program is free software; you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation; either version 2 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ */
+
+namespace Box\Model\Connection\Response;
+
+use Box\Model\Model;
+use Box\Model\Connection\Response\AuthenticationResponseInterface;
+
+class AuthenticationResponse extends Model implements AuthenticationResponseInterface
+{
+    protected $responseType;
+    protected $accessToken;
+    protected $expiresIn;
+    protected $tokenType;
+    protected $refreshToken;
+    protected $error;
+    protected $errorDescription;
+
+}
